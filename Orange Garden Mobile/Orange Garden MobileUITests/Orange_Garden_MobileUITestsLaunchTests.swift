d775e41133bb9ff37fc33ac079f76cc7bef5ce65
Cong Le <cle155@csu.fullerton.edu>
2025-04-06 19:51:52 -0700

    initial version

---
+++ Orange Garden Mobile/Orange Garden MobileUITests/Orange_Garden_MobileUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Orange_Garden_MobileUITestsLaunchTests.swift
+//  Orange Garden MobileUITests
+//
+//  Created by Cong Le on 4/6/25.
+//
+
+import XCTest
+
+final class Orange_Garden_MobileUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
